bb21bcf09a9e7745a73dc2a801a9b7320dbfde11
Luis Tabug <luistabug03@gmail.com>
2023-06-02 13:53:30 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-const divBoxEl = document.getElementById("box-container");
-const divBoxParentEl = document.querySelector("div.box-parent");
-
 const gameEl = document.getElementById("game");
 const homeEl = document.getElementById("home");
 const divBoxes = Array.from(
     document.querySelectorAll("div.box-container > div")
 );
+const emptyBoxes = divBoxes.filter((empty) => empty.innerHTML === "");
 const restartBtn = document.getElementById("restart");
 const quitBtn = document.getElementById("quit");
 const startBtn = document.getElementById("start-game");
@@ -13,20 +11,14 @@ const player1Turn = document.getElementById("player-1");
 const player2Turn = document.getElementById("player-2");
 const radioButtons = document.querySelectorAll('input[name="mode"]');
 
-const player1 = {
-    name: "",
-    symbol: "X",
-    isWinner: [],
-};
-const player2 = {
-    name: "",
-    symbol: "O",
-    isWinner: [],
-};
+const quitBtn2 = document.getElementById("quit-2");
+const restartBtn2 = document.getElementById("restart-2");
+const popUp = document.getElementById("pop-up");
 
 let gameMode = "singleplayer";
-let isGameActive = true;
-const turnArray = [];
+let isGameActive = false;
+let tiles = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
 const winCombination = [
     [0, 1, 2],
     [3, 4, 5],
@@ -40,37 +32,32 @@ const winCombination = [
 
 let index;
 
-function resize() {
-    const windowWidth = window.innerWidth * 0.95;
-
-    let boxHeight = window.innerHeight * 0.665;
-
-    if (boxHeight < windowWidth) {
-        divBoxEl.style.width = `${boxHeight}px`;
-        divBoxEl.style.height = `${boxHeight}px`;
-    } else {
-        divBoxEl.style.width = `${windowWidth}px`;
-        divBoxEl.style.height = `${windowWidth}px`;
-    }
-
-    console.log(window.innerHeight * 0.7, window.innerWidth);
+//
+// display: none
+function displayNone(gone) {
+    gone.classList.add("d-none");
+}
+function displayNoneRemove(appear) {
+    appear.classList.remove("d-none");
 }
 
-window.addEventListener("load", resize);
-window.addEventListener("resize", resize);
-
+//
+// restart button
 function restartGame() {
-    turnArray.length = 0;
+    tiles = tiles.map((tile) => (tile = ""));
     divBoxes.forEach((element) => {
         element.innerHTML = "";
     });
-    player1.isWinner.length = 0;
-    player2.isWinner.length = 0;
+    currentPlayer = "X";
     isGameActive = true;
     playerTurn(player2Turn, player1Turn);
+
+    drawOrWinner();
 }
 restartBtn.addEventListener("click", restartGame);
 
+//
+// multiplayer or singleplayer
 radioButtons.forEach((radioButton) => {
     radioButton.addEventListener("change", (event) => {
         gameMode = event.target.value;
@@ -78,10 +65,11 @@ radioButtons.forEach((radioButton) => {
     });
 });
 
+//
+// startButton
 function startGame() {
-    homeEl.style.display = "none";
-    gameEl.style.display = "block";
-    resize();
+    displayNone(homeEl);
+    displayNoneRemove(gameEl);
     player1.name = document.getElementById("player-1-input").value;
     player1Turn.innerHTML = player1.name;
     if (gameMode === "multiplayer") {
@@ -90,81 +78,74 @@ function startGame() {
     } else {
         player2Turn.innerHTML = "Bot";
     }
+    isGameActive = true;
 }
 startBtn.addEventListener("click", startGame);
 
+//
+// quitbutton
 function quitGame() {
-    homeEl.style.display = "block";
-    gameEl.style.display = "none";
+    displayNone(gameEl);
+    displayNoneRemove(homeEl);
     restartGame();
 }
 quitBtn.addEventListener("click", quitGame);
 
-function playerTurn(playerMove, nextPlayer) {
-    playerMove.classList.remove("animation");
-    nextPlayer.classList.add("animation");
-}
-
-function makeBotMove() {
-    const emptyBoxes = divBoxes.filter((box) => box.innerHTML === "");
-    if (emptyBoxes.length > 0) {
-        const randomIndex = Math.floor(Math.random() * emptyBoxes.length);
-        const botBox = emptyBoxes[randomIndex];
-        botBox.innerHTML = player2.symbol;
-        player2.isWinner.push(divBoxes.indexOf(botBox));
-        turnArray.push(player2.symbol);
-        playerTurn(player2Turn, player1Turn);
+//
+// show the player turn
+function playerTurn() {
+    if (currentPlayer === "X") {
+        player2Turn.classList.remove("animation");
+        player1Turn.classList.add("animation");
+    } else {
+        player1Turn.classList.remove("animation");
+        player2Turn.classList.add("animation");
     }
 }
 
-function click(box) {
-    let player1Win = winCombination.some((win) => {
-        return win.every((combination) => {
-            return player1.isWinner.includes(combination);
-        });
-    });
-
-    let player2Win = winCombination.some((win) => {
-        return win.every((combination) => {
-            return player2.isWinner.includes(combination);
-        });
-    });
-
-    if (player1Win === true && isGameActive === true) {
-        isGameActive = false;
-    } else if (player2Win === true && isGameActive === true) {
-        isGameActive = false;
-    } else if (
-        isGameActive === true &&
-        box.innerHTML === "" &&
-        (turnArray.at(-1) === "O" || turnArray.length === 0)
-    ) {
-        turnArray.push(player1.symbol);
-        box.innerHTML = player1.symbol;
-        player1.isWinner.push(divBoxes.indexOf(box));
-        playerTurn(player1Turn, player2Turn);
-
-        if (gameMode === "singleplayer") {
-            makeBotMove();
+//
+// win function
+function drawOrWinner() {
+    for (let condition of winCombination) {
+        const [a, b, c] = condition;
+        if (
+            tiles[a] === currentPlayer &&
+            tiles[b] === currentPlayer &&
+            tiles[c] === currentPlayer
+        ) {
+            return true;
         }
-    } else if (
-        isGameActive === true &&
-        box.innerHTML === "" &&
-        turnArray.at(-1) === "X"
-    ) {
-        turnArray.push(player2.symbol);
-        box.innerHTML = player2.symbol;
-        player2.isWinner.push(divBoxes.indexOf(box));
-        playerTurn(player2Turn, player1Turn);
     }
-
-    console.log(player1Win, player2Win);
 }
+
+//
+// game logic
+function click(event) {
+    const boxIndex = divBoxes.indexOf(event.target);
+
+    if (isGameActive && tiles[boxIndex] === "") {
+        tiles[boxIndex] = currentPlayer;
+        divBoxes.forEach((box, index) => {
+            box.innerHTML = tiles[index];
+        });
+
+        if (drawOrWinner()) {
+            isGameActive = false;
+        } else {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            playerTurn();
+        }
+    }
+    console.log(tiles, drawOrWinner());
+}
+
+// Event listener for each box element
 divBoxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        click(box);
-        // console.log(event.currentTarget);
+    box.addEventListener("click", (event) => {
+        click(event);
     });
 });
 
-gameEl.style.display = "none";
+//
+// after load the game is hidden
+window.addEventListener("load", displayNone(gameEl));
