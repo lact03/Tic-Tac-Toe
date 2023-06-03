2ee39036787f96a012523c3e649bd1b0357e6e18
Luis Tabug <luistabug03@gmail.com>
2023-06-03 16:13:16 +0800

    fix and add design

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const homeEl = document.getElementById("home");
 const divBoxes = Array.from(
     document.querySelectorAll("div.box-container > div")
 );
-const emptyBoxes = divBoxes.filter((empty) => empty.innerHTML === "");
+
 const restartBtn = document.getElementById("restart");
 const quitBtn = document.getElementById("quit");
 const startBtn = document.getElementById("start-game");
@@ -13,7 +13,9 @@ const radioButtons = document.querySelectorAll('input[name="mode"]');
 
 const quitBtn2 = document.getElementById("quit-2");
 const restartBtn2 = document.getElementById("restart-2");
-const popUp = document.getElementById("pop-up");
+const popUpEl = document.getElementById("pop-up");
+const scaleAnimationEl = document.querySelector("#pop-up > div");
+const messageEl = document.getElementById("message");
 
 let gameMode = "singleplayer";
 let isGameActive = false;
@@ -34,11 +36,8 @@ let index;
 
 //
 // display: none
-function displayNone(gone) {
-    gone.classList.add("d-none");
-}
-function displayNoneRemove(appear) {
-    appear.classList.remove("d-none");
+function display(element, className) {
+    element.classList.toggle(className);
 }
 
 //
@@ -50,34 +49,23 @@ function restartGame() {
     });
     currentPlayer = "X";
     isGameActive = true;
-    playerTurn(player2Turn, player1Turn);
-
-    drawOrWinner();
+    playerTurn();
 }
 restartBtn.addEventListener("click", restartGame);
 
 //
-// multiplayer or singleplayer
-radioButtons.forEach((radioButton) => {
-    radioButton.addEventListener("change", (event) => {
-        gameMode = event.target.value;
-        console.log(gameMode);
-    });
-});
-
-//
 // startButton
 function startGame() {
-    displayNone(homeEl);
-    displayNoneRemove(gameEl);
-    player1.name = document.getElementById("player-1-input").value;
-    player1Turn.innerHTML = player1.name;
+    display(homeEl, "d-none");
+    display(gameEl, "d-none");
+    player1Turn.innerHTML = document.getElementById("player-1-input").value;
+
     if (gameMode === "multiplayer") {
-        player2.name = document.getElementById("player-2-input").value;
-        player2Turn.innerHTML = player2.name;
+        player2Turn.innerHTML = document.getElementById("player-2-input").value;
     } else {
         player2Turn.innerHTML = "Bot";
     }
+
     isGameActive = true;
 }
 startBtn.addEventListener("click", startGame);
@@ -85,8 +73,8 @@ startBtn.addEventListener("click", startGame);
 //
 // quitbutton
 function quitGame() {
-    displayNone(gameEl);
-    displayNoneRemove(homeEl);
+    display(gameEl, "d-none");
+    display(homeEl, "d-none");
     restartGame();
 }
 quitBtn.addEventListener("click", quitGame);
@@ -105,7 +93,7 @@ function playerTurn() {
 
 //
 // win function
-function drawOrWinner() {
+function theresWinner() {
     for (let condition of winCombination) {
         const [a, b, c] = condition;
         if (
@@ -119,33 +107,112 @@ function drawOrWinner() {
 }
 
 //
+// draw function
+function isDraw() {
+    return tiles.every((tile) => tile !== "");
+}
+
+//
 // game logic
-function click(event) {
+function updateGameBoard() {
+    divBoxes.forEach((box, i) => {
+        box.innerHTML = tiles[i];
+    });
+}
+
+function endGame(message) {
+    isGameActive = false;
+    messageEl.innerHTML = message;
+    display(popUpEl, "d-none");
+    setTimeout(() => {
+        display(scaleAnimationEl, "scale-up");
+    }, 100);
+}
+
+function botAttack() {
+    const emptyTilesIndex = tiles.reduce((indices, tile, index) => {
+        if (tile === "") {
+            indices.push(index);
+        }
+        return indices;
+    }, []);
+
+    if (emptyTilesIndex.length > 0) {
+        const randomIndex = Math.floor(Math.random() * emptyTilesIndex.length);
+        const attackIndex = emptyTilesIndex[randomIndex];
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        tiles[attackIndex] = currentPlayer;
+    }
+}
+
+function attack(event) {
     const boxIndex = divBoxes.indexOf(event.target);
 
     if (isGameActive && tiles[boxIndex] === "") {
         tiles[boxIndex] = currentPlayer;
-        divBoxes.forEach((box, index) => {
-            box.innerHTML = tiles[index];
-        });
-
-        if (drawOrWinner()) {
-            isGameActive = false;
+        if (theresWinner()) {
+            endGame(`"${currentPlayer}" Won`);
+        } else if (isDraw()) {
+            endGame("Draw");
         } else {
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-            playerTurn();
+            if (gameMode === "multiplayer") {
+                currentPlayer = currentPlayer === "X" ? "O" : "X";
+                playerTurn();
+            } else if (gameMode === "singleplayer") {
+                botAttack();
+                if (theresWinner()) {
+                    endGame(`Bot Won`);
+                } else if (isDraw()) {
+                    endGame("Draw");
+                } else {
+                    currentPlayer = currentPlayer === "X" ? "O" : "X";
+                }
+            }
         }
+        updateGameBoard();
     }
-    console.log(tiles, drawOrWinner());
 }
 
+//
 // Event listener for each box element
 divBoxes.forEach((box) => {
     box.addEventListener("click", (event) => {
-        click(event);
+        attack(event);
     });
 });
 
 //
+// multiplayer or singleplayer
+radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener("change", (event) => {
+        gameMode = event.target.value;
+    });
+});
+
+//
+// pop up restart button
+restartBtn2.addEventListener("click", () => {
+    setTimeout(() => {
+        display(popUpEl, "d-none");
+    }, 300);
+
+    display(scaleAnimationEl, "scale-up");
+    restartGame();
+});
+
+//
+// pop up quit button
+quitBtn2.addEventListener("click", () => {
+    setTimeout(() => {
+        display(popUpEl, "d-none");
+    }, 300);
+    display(scaleAnimationEl, "scale-up");
+    quitGame();
+});
+
+//
 // after load the game is hidden
-window.addEventListener("load", displayNone(gameEl));
+window.addEventListener("load", () => {
+    display(gameEl, "d-none");
+    display(popUpEl, "d-none");
+});
